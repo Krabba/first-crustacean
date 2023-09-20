ce5b15032bf0ed4dd71813ec9701fdcad2af5a5d
Krabba <11424371+Krabba@users.noreply.github.com>
2023-09-20 12:45:48 +0200

    Pass a value to the function

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod xlog;
 fn main() {
     println!("Hello, world!");
 
-    xlog::log_x();
+    xlog::log_x(55);
 
     std::process::exit(0);
 }
--- src/xlog.rs
+++ src/xlog.rs
@@ -1,4 +1,4 @@
-pub fn log_x() {
-    let x = 43;
+pub fn log_x(value: i32) {
+    let x = value;
     println!("The value of x is: {}", x);
 }
